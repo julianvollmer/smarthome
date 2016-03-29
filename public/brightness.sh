487136ee1b59e774c4ef68129ef919be922a4f9c
Julian Vollmer <julianvollmer@gmail.com>
2016-03-29 22:56:07 +0200

    cleaning

--- public/brightness.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-brightness=$1
-
-openmilight "B0 F2 EA 73 $brightness 0E 70"
-openmilight "B0 F2 EA 73 $brightness 0E 71"
-openmilight "B0 F2 EA 73 $brightness 0E 72"
-openmilight "B0 F2 EA 73 $brightness 0E 73"
-openmilight "B0 F2 EA 73 $brightness 0E 74"
-
--- public/color.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-color=$1
-
-openmilight "B8 F2 EA $color 00 0F 70"
-openmilight "B8 F2 EA $color 00 0F 71"
-openmilight "B8 F2 EA $color 00 0F 72"
-openmilight "B8 F2 EA $color 00 0F 73"
-openmilight "B8 F2 EA $color 00 0F 74"
