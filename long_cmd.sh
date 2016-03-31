9e6a2323713f8f3b62b54c6b484b26238813c62a
Julian Vollmer <julianvollmer@gmail.com>
2016-03-31 22:03:33 +0200

    long_cmd added

---
+++ long_cmd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+button=$1
+
+for i in `seq 1 10`;
+do
+	value=$(<counter.txt)
+	((value+=3))
+	printf -v hex_result "%x" "$value"
+	
+	if [[ "$value" -gt "255"  ]]; then
+		((value-=256));
+	fi
+
+	if [[ "$value" -lt "16"  ]]; then
+		hex_result=0$hex_result;
+	fi
+
+	echo $hex_result
+	openmilight "B8 F2 EA 00 91 1$button $hex_result"
+	echo "$value" > counter.txt
+done
